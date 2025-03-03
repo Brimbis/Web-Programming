97fe560a88c8f39107ae4f2e66d12c184517b1e2
BHurt <brandonhurt208@gmail.com>
2025-03-03 17:42:29 -0600

    Functioning Mood Tracker

--- In-Class-Assignment-10/mood-tracker/src/App.js
+++ In-Class-Assignment-10/mood-tracker/src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      <MoodTracker></MoodTracker>
-    </div>
+    <>
+      <MoodTracker/>
+    </>
   );
 }
 
--- In-Class-Assignment-10/mood-tracker/src/MoodTracker.js
+++ In-Class-Assignment-10/mood-tracker/src/MoodTracker.js
@@ -1,57 +1,87 @@
 import React, { useState } from 'react';
+import moods from './Moods.json';
+import './MoodTracker.css';
 
 export default function MoodTracker() {
+    const [emoji, setEmoji] = useState(moods.happy.emoji);
+    const [color, setColor] = useState(moods.happy.color);
+    const [mainColor, setMainColor] = useState(moods.happy.maincolor);
+    const [description, setDescription] = useState(moods.happy.description);
+    const [image, setImage] = useState(moods.happy.image);
+    const [story, setStory] = useState(moods.happy.story);
 
-    const [mood, setMood] = useState("😁");
-
+    const handleMoodChange = (mood) => {
+        setEmoji(prevEmoji => prevEmoji = mood.emoji);
+        setColor(prevColor => prevColor = mood.color);
+        setMainColor(prevMainColor => prevMainColor = mood.maincolor);
+        setDescription(prevDescription => prevDescription = mood.description);
+        setImage(prevImage => prevImage = mood.image);
+        setStory(prevStory => prevStory = mood.story);
+    }
 
     return (
-        <div style={{textAlign:"center", padding:"20px"}}>
+        <>
+        <div style={{
+            textAlign:"center", 
+            padding:"20px", 
+            height: "100vh",
+            backgroundColor: color, 
+            transition: "0.5s", 
+            color: mainColor
+        }}>
             <h1>Mood Tracker</h1>
-            <p>Current Mood: {mood}</p>
+            <p>Current Mood: {emoji}</p>
             <button
-                onClick={()=>setMood("😁")}
+                onClick={()=>handleMoodChange(moods.happy)}
                 style={{margin:"5px",
                         padding:"10px", 
-                        background: "lightgreen"
+                        background: moods.happy.maincolor
                 }}
             >Happy</button>
             <button
-                onClick={()=>setMood("🤣")}
+                onClick={()=>handleMoodChange(moods.excited)}
                 style={{margin:"5px",
                         padding:"10px", 
-                        background: "lightblue"
+                        background: moods.excited.maincolor
                 }}
-            >Laughing</button>
+            >Excited</button>
             <button
-                onClick={()=>setMood("😒")}
+                onClick={()=>handleMoodChange(moods.bored)}
                 style={{margin:"5px",
                     padding:"10px", 
-                    background: "gray"
+                    background: moods.bored.maincolor
             }}
             >Bored</button>
             <button
-                onClick={()=>setMood("😎")}
+                onClick={()=>handleMoodChange(moods.cool)}
                 style={{margin:"5px",
                     padding:"10px", 
-                    background: "red"
+                    background: moods.cool.maincolor
             }}
             >Cool</button>
             <button
-                onClick={()=>setMood("😍")}
-                style={{margin:"5px",
-                    padding:"10px", 
-                    background: "pink"
+                onClick={()=>handleMoodChange(moods.loving)}
+                value="Loving"
+                style={{
+                    background: moods.loving.maincolor
             }}
             >Loving</button>
             <button
-                onClick={()=>setMood("😢")}
+                onClick={()=>handleMoodChange(moods.sad)}
                 style={{margin:"5px",
                     padding:"10px", 
-                    background: "darkblue", 
-                    color: "white"
+                    background: moods.sad.maincolor
             }}
             >Sad</button>
+
+            <h2>Description</h2>
+            <p>{description}</p>
+
+            <img src={image} alt=''/>
+
+            <h2>Short Story</h2>
+            <p>{story}</p>
         </div>
+        </>
     );
 }

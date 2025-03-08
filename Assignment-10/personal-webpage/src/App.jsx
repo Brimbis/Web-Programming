6194bc561a326f9d338d80da8d99b2b0168324c0
BHurt <brandonhurt208@gmail.com>
2025-03-07 18:16:47 -0600

    Update App.jsx
    updated styling

--- Assignment-10/personal-webpage/src/App.jsx
+++ Assignment-10/personal-webpage/src/App.jsx
@@ -2,48 +2,48 @@ import { useState } from 'react'
 import './App.css'
 import Logo from './Logo.jsx';
 
-
 export default function App() {
 
   return (
     <>
-      <div className='font-mono text-blue-100 bg-indigo-950 h-screen flex flex-col'>
-        <header className='bg-indigo-400 text-7xl mt-10 w-full text-center flex justify-center flex-col border-t-8 border-b-8 border-indigo-500 shadow-2xl'>
-        <img className='max-h-52 max-w-52 mt-10 self-center rounded-full shadow-2xl' src='./src/assets/profile-image.png' alt='profile-image'></img>
-          <h1 className='p-7 border-b-4 border-dashed border-indigo-300'>Brandon Hurt</h1>
-          <div className='flex justify-center m-5'>
-            <Logo
-              link='https://www.linkedin.com/in/brandon-hurt-49321a354/' 
-              image='./src/assets/linkedin-logo.png'
-              styling='hover:opacity-70 max-h-15 pr-10 hover:scale-110 transition'
-            />
-            <Logo
-              link='https://github.com/Brimbis'
-              image='./src/assets/github-logo.png'
-              styling='hover:opacity-70 max-h-15 pr-10 hover:scale-110 transition'
-            />
-            <Logo
-              link='https://discordapp.com/users/263572671424495616'
-              image='./src/assets/discord-logo.png'
-              styling='hover:opacity-70 max-h-18 pr-10 hover:scale-110 transition'
-            />
-            <Logo
-              link='mailto:brandonhurt208@gmail.com'
-              image='./src/assets/gmail-logo.png'
-              styling='hover:opacity-70 max-h-20 hover:scale-110 transition'
-            />
-          </div>
-        </header>
-        <main className='grid flex-row mt-15 grid-cols-3'>
-          <h2 className='text-4xl mb-20 border-l-4 m-6 p-6'>Biography:</h2>
-          <p className='col-span-2 text-xl p-6 m-6 text-center'>Hello! My name is Brandon Hurt and I am from Russellville,  Arkansas. I am currently a Senior at Arkansas Tech Univeristy, and my love for all things technology has led me this far.<br/><br/>I love video games: Some of my favorites include Geometry Dash, Outer Wilds, and Ori. In Geometry Dash I have several projects which can be found on my <a className='text-blue-400 cursor-pointer hover:opacity-70' href='https://www.youtube.com/@brimbis7104'>YouTube</a>.<br/><br/>I love to meet new people and learn new things so feel free to reach out to me!</p>
-
-          <h2 className='text-4xl mb-20 border-l-4 m-6 p-6'>Education:</h2>
-
-        </main>
-        <footer>
-
-        </footer>
+    <div className='h-screen w-screen bg-indigo-950 flex justify-center items-center'>
+        <div className='font-mono text-blue-100 flex flex-col items-center max-w-4xl w-full'>
+          <header className='bg-indigo-400 text-7xl mt-10 w-screen text-center flex flex-col items-center border-t-8 border-b-8 border-indigo-500 shadow-2xl'>
+            <img className='max-h-52 max-w-52 mt-10 rounded-full shadow-2xl hover:-rotate-20 hover:scale-110 transition' src='./src/assets/profile-image.png' alt='profile-image'/>
+            <h1 className='p-7 border-b-4 border-dashed border-indigo-300'>Brandon Hurt</h1>
+            <div className='flex justify-center m-5'>
+              <Logo
+                link='https://www.linkedin.com/in/brandon-hurt-49321a354/' 
+                image='./src/assets/linkedin-logo.png'
+                styling='hover:opacity-70 max-h-15 pr-10 hover:scale-110 transition'
+              />
+              <Logo
+                link='https://github.com/Brimbis'
+                image='./src/assets/github-logo.png'
+                styling='hover:opacity-70 max-h-15 pr-10 hover:scale-110 transition'
+              />
+              <Logo
+                link='https://discordapp.com/users/263572671424495616'
+                image='./src/assets/discord-logo.png'
+                styling='hover:opacity-70 max-h-18 pr-10 hover:scale-110 transition'
+              />
+              <Logo
+                link='mailto:brandonhurt208@gmail.com'
+                image='./src/assets/gmail-logo.png'
+                styling='hover:opacity-70 max-h-20 hover:scale-110 transition'
+              />
+            </div>
+          </header>
+          <main className='w-full max-w-3xl grid grid-cols-3 items-start p-5 m-10 bg-indigo-900 rounded-2xl shadow-2xl'>
+            <h2 className='text-4xl border-l-4 p-6 w-full self-start'>Biography:</h2>
+            <p className='text-xl p-3 text-center col-span-2'>Hello! My name is Brandon Hurt and I am from Russellville, Arkansas. I am currently a Senior at Arkansas Tech University, and my love for all things technology has led me this far.<br/><br/>I love video games: Some of my favorites include Geometry Dash, Outer Wilds, and Ori. In Geometry Dash I have several projects which can be found on my <a className='text-blue-400 cursor-pointer hover:opacity-70' href='https://www.youtube.com/@brimbis7104'>YouTube</a>.<br/><br/>I love to meet new people and learn new things so feel free to reach out to me!</p>
+            <h2 className='text-4xl border-l-4 p-6 w-full self-start text-center'>Education:</h2>
+            <p className='text-xl p-3 text-center col-span-2'>content</p>
+          </main>
+          <footer className='mt-10'>
+            
+          </footer>
+        </div>
       </div>
     </>
   )

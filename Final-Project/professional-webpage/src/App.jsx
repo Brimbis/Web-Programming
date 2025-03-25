f18618f2c19a7b80c6c5ff8bf09e38f80edb2f95
bhurt <brandonhurt208@gmail.com>
2025-03-24 19:40:07 -0500

    Final Project Initial Setup
    Needs further setup regarding backend / database integration.

---
+++ Final-Project/professional-webpage/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+import AboutMe from './AboutMe.jsx';
+
+export default function App() {
+  return (
+    <>
+      <AboutMe/>
+    </>
+  );
+}
---
+++ Final-Project/professional-webpage/src/Logo.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+export default function Logo({link, image, styling}) {
+
+    return (
+        <>
+            {link !== '' ? ( // Ternary Operator for image switching
+                <a href={link} title={link}>
+                    <img src={image} className={styling}/>
+                </a>
+            ) : (
+                <img src={image} className={styling}/>
+            )}
+        </>
+    );
+}

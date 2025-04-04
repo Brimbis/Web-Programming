8693b1b0e58c2468db260f2aa6cb1719597eee4c
bhurt <brandonhurt208@gmail.com>
2025-04-04 14:02:54 -0500

    Database integration

--- Day-17/projects-app/server.js
+++ Day-17/projects-app/server.js
@@ -31,5 +31,21 @@ app.get("/projects", async (req, res) => {
     }
 });
 
+app.post("/projects", async(req, res) => {
+    try {
+        const {project_name, description, date_completed, technologies_used, project_url} = req.body;
+
+        // Insert Data
+        const result = await pool.query(
+            "INSERT INTO completed_projects (project_name, description, date_completed, technologies_used, project_url) VALUES ($1, $2, $3, $4, $5) RETURNING *", 
+            [project_name, description, date_completed, technologies_used, project_url]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send("Server Error!");
+    }
+});
+
 // Start Server
 app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
